5286e1e40b8b5f092dedf29a42db173e0128bc60
Antônio Prado <antonioeliaspr@gmail.com>
2025-09-30 08:54:30 -0300

    feat: configure Angular routing with router-outlet and pages

--- frontend-gestao-marketplace/src/app/app.routes.ts
+++ frontend-gestao-marketplace/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Products } from './pages/products/products';
+import { Layout } from './pages/layout/layout';
+import { NewProduct } from './pages/new-product/new-product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login,
+    
+    },
+    {
+        path: '',
+        component: Layout,
+        canActivateChild: [],
+        children: [
+            {
+                path: 'products',
+                component: Products
+            },
+            {
+                path: 'new-product',
+                component: NewProduct
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/login'
+    }
+];
